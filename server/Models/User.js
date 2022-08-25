aecdc79fbc4d957d5a738c9f91065d46151e1918
Felicity Brammer <felicitybrammer@gmail.com>
2022-08-24 19:52:02 -0700

    user model and associations, including bcrypt

---
+++ server/Models/User.js
@@ -0,0 +1,60 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');  //check path
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init({
+    //define model attributes
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey:true, 
+        autoIncrement: true,
+
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6]
+        }
+    }
+}, {
+
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10)
+            return newUserData
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+            return updatedUserData
+        }
+    },
+}, 
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'User'
+});
+
+module.exports = User;
