5c045359cfa1791e99e3cfa293a0f19fa4961718
Felicity Brammer <felicitybrammer@gmail.com>
2022-08-25 10:58:02 -0700

    adding faker data to make pet objects

---
+++ server/seeders/seeds.js
@@ -0,0 +1,55 @@
+const { faker } = require('@faker-js/faker');
+
+const db = require('../config/connection');
+
+const { Pet } = require('../Models');
+
+db.once('open', () => {
+
+    let petData = []
+
+    for (let id = 1; id <= 100; id++) {
+
+        let sex = faker.helpers.arrayElement(['female', 'male']);
+        let name = faker.name.firstName(sex);
+        let description = faker.lorem.words(20);
+        let location = faker.address.cityName();
+        let ageCategory = faker.helpers.arrayElement(["baby", "adult", "senior"]);
+        let category = faker.helpers.arrayElement(["Dog", "Cat", "Bird", "Reptile"]);
+       
+        let image;
+        if (category == "Cat") {
+             image = faker.image.cats(600, 400, true);
+        } else if (category == "Dog") {
+             image = faker.image.imageUrl(600, 400, 'dog', true);
+        } else if (category == "Bird") {
+            image = faker.image.imageUrl(600, 400, 'bird', true);
+        } else {
+            image = faker.image.imageUrl(600, 400, 'snake' || 'lizard' || 'gecko', true);
+        }
+
+        let needs = faker.datatype.boolean();
+        let household = faker.helpers.arrayElement(["baby", "child", "adult", "senior"]);
+        let other_pets = faker.datatype.boolean();
+
+        pets.push({
+            "id": id,
+            "name": name,
+            "sex": sex, 
+            "image": image,
+            "description": description,
+            "location": location,
+            "ageCategory": ageCategory,
+            "category": category,
+            "needs": needs,
+            "household": household,
+            "other_pets": other_pets
+        });
+    }
+
+    const createdPets = await Pet.collection.insertMany(petData);
+
+    console.log('all done');
+    process.exit(0);
+
+})
