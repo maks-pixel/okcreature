c18ed2fb2e20ea5c2b50d3d5cd85490e819c4040
Makda Girmay <makdagirmay4@gmail.com>
2022-08-25 13:51:38 -0400

    config added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
